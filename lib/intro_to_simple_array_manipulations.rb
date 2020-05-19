e3adbf1c4324f40f1c7da8f70832184e6cc3aaa9
Julio Chazari <julio.chazari@gmail.com>
2020-05-19 06:07:55 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,10 +10,14 @@ def using_uniq(array)
   array.uniq
 end
 
+def using_flatten(array)
+  array.flatten
+end
+
 def using_delete(array, string)
   array.delete(string)
 end
 
 def using_delete_at(array, integer)
-  array.delet_at(integer)
+  array.delete_at(integer)
 end
